5f05bbbc2611657b443f6af2758f8d0498c374cb
Palantir1997 <palantir1997@gmail.com>
2026-06-01 17:41:22 +0900

    admin메뉴 권한 있어야 보이게

--- saessak/admin/include/login_process.php
+++ saessak/admin/include/login_process.php
@@ -4,10 +4,10 @@ session_start();
 $user_id = $_POST['user_id'];
 $password = $_POST['password'];
 
-// 로그인 성공 시
 if ($user_id === "admin" && $password === "1234") {
-    $_SESSION['user_id'] = $user_id; // 세션 데이터 저장
-    header("Location: ../dashboard.php"); // 대시보드로 이동
+    $_SESSION['user_id'] = $user_id;
+    $_SESSION['role'] = 'admin'; // ← 이 한 줄만 추가
+    header("Location: ../dashboard.php");
     exit();
 } else {
     echo "<script>alert('로그인 실패'); history.back();</script>";
