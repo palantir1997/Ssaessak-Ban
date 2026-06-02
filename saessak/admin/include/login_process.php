beb56084f5706d4a35e5c1f3a3b96c6777533c1d
Palantir1997 <palantir1997@gmail.com>
2026-06-02 15:06:46 +0900

    권한수정

--- saessak/admin/include/login_process.php
+++ saessak/admin/include/login_process.php
@@ -8,10 +8,8 @@ $password = $_POST['password'] ?? '';
 if ($user_id === 'admin' && $password === '1234') {
     $_SESSION['user_id'] = $user_id;
     $_SESSION['admin_id'] = $user_id;
+    $_SESSION['role'] = 'admin'; // ← 이거 추가!
     header('Location: ../DashBoard.php');
-    exit();
-} else {
-    echo "<script>alert('로그인 실패'); history.back();</script>";
     exit();
 }
 ?>
