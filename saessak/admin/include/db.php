eef800c59483b821c875211ba462b8475aff4d05
Palantir1997 <palantir1997@gmail.com>
2026-06-02 11:11:14 +0900

    db연동 테스트

--- saessak/admin/include/db.php
+++ saessak/admin/include/db.php
@@ -1,4 +1,7 @@
 <?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 $conn = mysqli_connect("localhost", "root", "", "saessak");
 if (!$conn) {
     die("DB 연결 실패: " . mysqli_connect_error());
